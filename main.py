6a881908f0c76e96f4e373b4fb0775631bdc9539
0x1057 <0x1057.main@gmail.com>
2025-02-05 19:23:32 -0600

    Starting up Asteroids project

--- main.py
+++ main.py
@@ -7,12 +7,18 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    ticker = pygame.time.Clock()
+    dt = 0
+
+    # Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        ticker.tick(60)
+        dt = ticker.get_time() / 1000
 
 if __name__ == "__main__":
     main()
---
+++ player.py
@@ -0,0 +1,19 @@
+import pygame
+from circleshape import *
+from constants import PLAYER_RADIUS
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        super().__init__(self.x, self.y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    # player ship method
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.rotation / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
